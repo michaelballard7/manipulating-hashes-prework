b0e658c7435f74890e4e28604c66734a393145b0
Michael Ballard <ballard.michael9@gmail.com>
2018-05-09 14:43:45 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,9 +13,13 @@ def first_challenge
     }
   }
 
-  #your code here
-
-
+  contacts.each do |key, value|
+    key.each do |attribute, value|
+      if attribute == :favorite_icecream_flavors
+         value.delete_if {|value| value == "strawberry"}
+      end
+    end
+  end
   #remember to return your newly altered contacts hash!
   contacts
 end
